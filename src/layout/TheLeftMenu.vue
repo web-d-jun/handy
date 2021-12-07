351e21465654509cb9e6e66ed1a77bb69c0afb0b
junyoung <cjy874545@gamil.com>
2021-12-08 00:59:29 +0900

    update

--- src/layout/TheLeftMenu.vue
+++ src/layout/TheLeftMenu.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="w-1/5 h-full flex flex-col left-menu sticky h-screen overflow-y-auto left-0 top-0 bg-gray-900">
-    <div class="user-info">cho jun young cjy874545@gmail.com</div>
-    <div v-for="(item, index) in menu" :key="index">
-      <AppButton v-bind="item" />
+  <div class='w-1/5 h-full flex flex-col left-menu sticky h-screen overflow-y-auto left-0 top-0 bg-gray-900'>
+    <div class='user-info'>
+      <img src='/image/logo_iam.png' class='image-logo' alt=''>
+      cho jun young cjy874545@gmail.com
+    </div>
+    <div v-for='(item, index) in menu' :key='index'>
+      <AppButton v-bind='item' />
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { defineComponent } from 'vue';
 import AppButton from '@/components/AppButton.vue';
 import Menu from '@/ts/menu';
@@ -26,7 +29,11 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
 .left-menu {
+  & .image-logo {
+    width: 50px;
+    border-radius: 50%;
+  }
 }
 </style>
