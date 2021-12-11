9654a86f86c5124a6a3c29d612515103b3638d53
junyoung <cjy874545@gmail.com>
2021-12-12 00:28:47 +0900

    update

--- src/layout/TheHeader.vue
+++ src/layout/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
-    <div class="w-full p-3 shadow">
-      <button class="bar-btn btn-active rounded-full w-10 h-10 hover:bg-black hover:bg-opacity-20 opacity-70">
+    <div class="w-full p-2 shadow">
+      <button class="bar-btn btn-active rounded-full w-8 h-8 hover:bg-black hover:bg-opacity-20 opacity-70">
         <FontAwesomeIcon :icon="['fas', 'bars']" />
       </button>
     </div>
