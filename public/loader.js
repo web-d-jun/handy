8651fa36034c4e74f4ce88cc2fbbcd9aa96cce68
junyoung <cjy874545@gmail.com>
2021-12-18 00:43:24 +0900

    update

--- public/loader.js
+++ public/loader.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  const loader = document.querySelector('.loader-inner');
+  const loader = document.querySelector('.loader-container');
   loader.style.display = 'none';
   console.log(loader);
 };
