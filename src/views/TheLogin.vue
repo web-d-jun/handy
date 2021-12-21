35b91a0e3af5edef7407632f8f35c4a937122268
junyoung <cjy874545@gmail.com>
2021-12-22 01:55:15 +0900

    update

--- src/views/TheLogin.vue
+++ src/views/TheLogin.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>login</div>
+  <div class="flex h-screen">
+    <div class="login-contents flex-initial w-3/5"></div>
+    <div class="image-contents flex-initial w-2/5"></div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import faker from 'faker';
 
 export default defineComponent({
   name: 'Login',
+  setup() {
+    console.log(faker.image.fashion())
+    return {};
+  },
 });
 </script>
+<style scoped>
+.login-contents {
+  border: 1px solid red;
+}
+
+.image-contents {
+  border: 1px solid blue;
+}
+</style>
