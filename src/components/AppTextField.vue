1032a7a66760f03b6d2aefd128b352de61302204
junyoung <cjy874545@gmail.com>
2021-12-22 10:07:28 +0900

    update

---
+++ src/components/AppTextField.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='app-text-field w-full py-3'>
+    <input type="text" class="app-input w-full h-9" />
+  </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppTextField',
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.app-input {
+  @apply rounded;
+  border: 0;
+  outline: 1px solid $fourth;
+
+  &:focus {
+    outline: 1px solid $third;
+  }
+}
+</style>
--- src/views/TheLogin.vue
+++ src/views/TheLogin.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="flex h-screen">
-    <div class="login-contents flex-initial w-3/5"></div>
+    <div class="login-contents flex-initial w-3/5 md:p-16 sm:p-5 text-right">
+      <div class="login h-full w-1/2 ml-auto">
+        <AppTextField />
+      </div>
+    </div>
     <div class="image-contents flex-initial w-2/5"></div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-import faker from 'faker';
+// import faker from 'faker';
+import AppTextField from '@/components/AppTextField.vue';
 
 export default defineComponent({
   name: 'Login',
+  components: {
+    AppTextField,
+  },
   setup() {
-    console.log(faker.image.fashion())
+    // console.log(faker.image.fashion())
     return {};
   },
 });
 </script>
 <style scoped>
 .login-contents {
+  border: 1px solid red;
+}
+
+.login {
   border: 1px solid red;
 }
 
