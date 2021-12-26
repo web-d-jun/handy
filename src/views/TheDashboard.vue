44d9e3e5e92d8bd2701dffa17e173fb4a214d9ed
junyoung <cjy874545@gmail.com>
2021-12-26 16:14:26 +0900

    chat example

--- src/views/TheDashboard.vue
+++ src/views/TheDashboard.vue
@@ -3,10 +3,16 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { io } from 'socket.io-client';
 
 export default defineComponent({
   name: 'Dashboard',
   setup() {
+    const socket = io('ws://localhost:3000', {});
+    socket.emit('chat message', 'test message');
+    socket.on('chat message', msg => {
+      console.log(msg,'client');
+    });
     return {};
   },
 });
