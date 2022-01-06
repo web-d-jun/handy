344b6e6c59c1eaaabb0670a8a98b4bd82db54a9f
junyoung <cjy874545@gmail.com>
2022-01-06 23:42:03 +0900

    menus toggle

--- src/components/AppMenus.vue
+++ src/components/AppMenus.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="app-menus toggle" :class="{ show: showList }" @click="showList = !showList">
-    <slot name="button" :user="{ test: 11 }"></slot>
-    <slot></slot>
+  <div class="app-menus toggle" :class="{ show: showList }">
+    <div @click="toggle">
+      <slot name="button"></slot>
+    </div>
+    <div @click="toggle">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -11,7 +15,26 @@ export default defineComponent({
   name: 'AppMenus',
   setup(props) {
     const showList = ref(false);
-    return { props, showList };
+
+    const toggleListenerEvent = ($event: Event) => {
+      const elem = $event.target as HTMLDivElement;
+      const target = document.querySelector('.app-menus.toggle') as HTMLDivElement;
+      if (target !== elem && !target.contains(elem)) {
+        showList.value = false;
+        document.removeEventListener('click', toggleListenerEvent);
+      }
+    };
+
+    const toggle = () => {
+      showList.value = !showList.value;
+      if (showList.value) {
+        document.addEventListener('click', toggleListenerEvent);
+      } else {
+        document.removeEventListener('click', toggleListenerEvent);
+      }
+    };
+
+    return { props, showList, toggle };
   },
 });
 </script>
@@ -20,12 +43,11 @@ export default defineComponent({
   @apply relative;
   &.show {
     ::v-slotted(.list-contents) {
-      opacity: 1;
+      @apply block;
     }
   }
   ::v-slotted(.list-contents) {
-    @apply absolute right-0 px-3 bg-white shadow rounded;
-    opacity: 0;
+    @apply absolute right-0 px-3 bg-white shadow rounded hidden;
     .menu-list {
       .item {
         @apply w-auto h-9 leading-9 whitespace-nowrap cursor-pointer text-sm;
